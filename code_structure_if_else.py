926531e10f776f4d0e2d9e536f7e963636904abf
Tudaozi <1626202029@qq.com>
2019-02-09 13:56:31 +0800

    学习完成第十三课

--- code_structure_if_else.py
+++ code_structure_if_else.py
@@ -33,12 +33,12 @@ if height > 180:
     print('height')
 elif height < 170:
     print('no height')
-elif 170<height<175:
+elif 170 < height < 175:
     print('no height')
 else:
     print('no no no height')
 
-#常见的比较判断操作符
+# 常见的比较判断操作符
 '''
 ==     相等              ！=    不等于
 <        小于             <=     小于等于
@@ -47,25 +47,58 @@ in       属于（被包含）
 通过返回的布尔值（True或False）来判断
 '''
 
+# 判断两个元素是否相等，如果相等输出True，如果不相等输出False
+a = 1
+b = 1
+if a == b:
+    print('True')
+else:
+    print('False')
 
+# 判断a是否等于b，如果相等输出True，如果不相等输出False
+a = 1
+b = 1
+if a != b:
+    print('True')
+else:
+    print('False')
 
+# 判断a是否小于b，如果小于输出True，如果不小于输出False
+a = 1
+b = 2
+if a < b:
+    print('True')
+else:
+    print('False')
 
+# 判断a元素是否在b列表中，如果是输出True，如果不是输出False
+a = 1
+b = [1, 2, 3]
+if a in b:
+    print('True')
+else:
+    print('False')
 
-
-#布尔操作符
+# 布尔操作符
 '''
 and
 or
 not
 '''
 
-
-
-
-#关于True和False
+# 关于True和False
 '''
 以下情况属于False：
 None
 0     和   0.0
 ‘’、 []  、()、{}、set()
 '''
+
+print(bool())
+print(bool([]))
+print(bool(0))
+print(bool(0.0))
+print(bool(''))
+print(bool(()))
+print(bool({}))
+print(bool(set()))
