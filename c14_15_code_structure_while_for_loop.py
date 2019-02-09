b596f17d93a1887200b7221b62432fea3dcba21c
Tudaozi <1626202029@qq.com>
2019-02-09 15:35:28 +0800

    创建14、15课程

---
+++ c14_15_code_structure_while_for_loop.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: Tudaozi
+@contact: tudaozi@126.com
+@project: pystudy
+@file: c14_15_code_structure_while_for_loop.py
+@time: 2019-02-09 15:33
+@desc:
+'''
