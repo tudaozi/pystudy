f401500d4a0f98d2a7e30917e79551329a971e1a
Tudaozi <1626202029@qq.com>
2019-02-10 17:24:25 +0800

    新建数据类型和数据结构文件简介

---
+++ b00_data_type.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: Tudaozi
+@contact: tudaozi@126.com
+@project: pystudy
+@file: b00_data_type.py
+@time: 2019-02-10 17:00
+@desc:
+'''
+
+# 元素
+elem1 = 'a'
+elem2 = 1
+
+# 字符串
+str1 = 'abc'
+str2 = '123'
---
+++ b01_data_structure.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: Tudaozi
+@contact: tudaozi@126.com
+@project: pystudy
+@file: b01_data_structure.py
+@time: 2019-02-10 17:00
+@desc:
+'''
+
+# 列表
+list1 = [1, 2, 3]
+list2 = ['a', 'b', 'c']
+
+# 字典
+dict1 = {1: 1, 2: 2, 3: 3}
+dict2 = {'a': 1, 'b': 2, 'c': 3}
+
+# 元组
+tuple1 = (1, 2, 3,)
+tuple2 = ('a', 'b', 'c',)
+
+# 集合
+set1 = {1, 2, 3}
+set2 = {'a', 'b', 'c'}
