0d6f7696ddc80837f686c6b23309f521cb76104f
Tudaozi <46038475+tudaozi@users.noreply.github.com>
2019-03-25 13:31:12 +0800

    练习

--- c14_15_16code_structure_while_for_loop.py
+++ c14_15_16code_structure_while_for_loop.py
@@ -1,13 +1,4 @@
-#!/usr/bin/env python
-# encoding: utf-8
-'''
-@author: Tudaozi
-@contact: tudaozi@126.com
-@project: pystudy
-@file: c14_15_16code_structure_while_for_loop.py
-@time: 2019-02-09 15:33
-@desc:
-'''
+
 
 # 循环与迭代
 # while循环
